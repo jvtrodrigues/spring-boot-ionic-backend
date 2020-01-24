d6c44d9e7dfee09005c5cdcb7d181130db60a579
José Rodrigues <josehgunner@gmail.com>
2020-01-24 05:23:02 -0300

    Paginacao com paramentros opcionais na requisicao

--- src/main/java/com/sama/cursomc/services/DBService.java
+++ src/main/java/com/sama/cursomc/services/DBService.java
@@ -63,7 +63,12 @@ public class DBService {
 	public void instantiateTestDataBase() throws ParseException {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		Categoria cat3 = new Categoria(null,  "Programação");
+		Categoria cat3 = new Categoria(null,  "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null,  "Eletrônicos");
+		Categoria cat5 = new Categoria(null,  "Jardinagem");
+		Categoria cat6 = new Categoria(null,  "Decoração");
+		Categoria cat7 = new Categoria(null,  "Perfumaria");
+		
 		Produto p1 = new Produto(null, "Computador", 2000.0);
 		Produto p2 = new Produto(null, "Impressora", 800.0);
 		Produto p3 = new Produto(null, "Mouse", 80.0);
@@ -75,7 +80,7 @@ public class DBService {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		catRepo.saveAll(Arrays.asList(cat1, cat2, cat3));
+		catRepo.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		prodRepo.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
