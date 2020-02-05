bd3e5f702626fa33bff84c5a4346ea805076ed77
José Rodrigues <josehgunner@gmail.com>
2020-02-05 19:44:20 -0300

    Restricao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/sama/cursomc/services/ClienteService.java
+++ src/main/java/com/sama/cursomc/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.sama.cursomc.domain.Cidade;
 import com.sama.cursomc.domain.Cliente;
 import com.sama.cursomc.domain.Endereco;
+import com.sama.cursomc.domain.enuns.Perfil;
 import com.sama.cursomc.domain.enuns.TipoCliente;
 import com.sama.cursomc.dto.ClienteDTO;
 import com.sama.cursomc.dto.ClienteNewDTO;
 import com.sama.cursomc.repositories.ClienteRepository;
 import com.sama.cursomc.repositories.EnderecoRepository;
+import com.sama.cursomc.security.UserSS;
+import com.sama.cursomc.services.exceptions.AuthorizationException;
 import com.sama.cursomc.services.exceptions.DataIntegrityException;
 import com.sama.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -36,6 +39,13 @@ public class ClienteService {
 	private BCryptPasswordEncoder pe;
 	
 	public Cliente find(Integer id) {
+		
+		UserSS user = UserService.authenticated();
+		
+		if(user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
