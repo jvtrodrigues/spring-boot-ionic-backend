126e4b6d566121d8567748384b0c5e9179a36fb9
José Rodrigues <josehgunner@gmail.com>
2020-03-17 02:05:21 -0300

    Salvando primeiro arquivo no s3

--- src/main/java/com/sama/cursomc/CursomcApplication.java
+++ src/main/java/com/sama/cursomc/CursomcApplication.java
@@ -1,19 +1,26 @@
 package com.sama.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sama.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\Users\\sama\\Downloads\\bomdia.png");
 	}
 	
 }
